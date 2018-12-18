16aec04ee362e26a57663496707737e008702136
guomengmeng <YS.guomengmeng@h3c.com>
2018-12-18 14:37:48 +0800

    Merge remote-tracking branch 'remotes/origin/master' into developer
    添加一个vue文件的搭建：https://segmentfault.com/a/1190000012015667

---
+++ vue_router_elementui/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+
+    <el-container>
+      <el-header>
+        <mheader class="header"></mheader>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <navmenu></navmenu>
+        </el-aside>
+        <el-main><router-view></router-view></el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import NavMenu from '@/components/NavMenu'
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    'navmenu': NavMenu,
+    'mheader': Header
+  }
+}
+</script>
+
+<style>
+.header {
+  background-color: #409EFF;
+  color: #fff;
+  line-height: 60px;
+}
+</style>
---
+++ vue_router_elementui/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-row>
+      <el-col :span="24">
+        <div class="header-wrap">Element</div>
+      </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header-wrap{
+
+}
+</style>
